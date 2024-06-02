0e8f16d4844560d9f013bac9eb16a55203a14c24
Leonardo <leonardocavenago05@gmail.com>
2024-06-02 05:13:12 +0200

    Allow N-many users, and a way to exclude specific headings (#1)
    * Allow N-many users, and exclude headings
    
    * Disable debug mode

--- anilist-cmp/__init__.py
+++ anilist-cmp/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+from functools import reduce
+from operator import or_, and_
 from enum import Enum
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Sequence
 
 import httpx
 from litestar import Litestar, MediaType, Response, get, status_codes
@@ -10,39 +12,28 @@ from litestar.middleware.rate_limit import RateLimitConfig
 if TYPE_CHECKING:
     from .types_.responses import AnilistError, AnilistErrorResponse, AnilistResponse, InnerMediaEntry, MediaEntry
 
+USER_QUERY = """
+user{number}: MediaListCollection(userName: $username{number}, status: $status, type: ANIME) {{
+    lists {{
+        entries {{
+            media {{
+                id
+                title {{
+                    romaji
+                    english
+                    native
+                }}
+                siteUrl
+            }}
+        }}
+    }}
+}}
+"""
+
 QUERY = """
-query ($username1: String, $username2: String, $status: MediaListStatus) {
-    user1: MediaListCollection(userName: $username1, status: $status, type: ANIME) {
-        lists {
-            entries {
-                media {
-                    id
-                    title {
-                        romaji
-                        english
-                        native
-                    }
-                    siteUrl
-                }
-            }
-        }
-    }
-    user2: MediaListCollection(userName: $username2, status: $status, type: ANIME) {
-        lists {
-            entries {
-                media {
-                    id
-                    title {
-                        romaji
-                        english
-                        native
-                    }
-                    siteUrl
-                }
-            }
-        }
-    }
-}
+query ({parameters}, $status: MediaListStatus) {{
+    {subqueries}
+}}
 """
 
 OPENGRAPH_HEAD = """
@@ -51,38 +42,36 @@ OPENGRAPH_HEAD = """
 <head>
     <meta charset="UTF-8" />
     <title>Hi</title>
-    <meta property="og:title" content="{user1} and {user2}'s mutual '{status}' anilist entries." />
-    <meta property="og:description" content="They both currently have {mutual} mutual entries." />
+    <meta property="og:title" content="Common '{status}' anilist entries for {users}." />
+    <meta property="og:description" content="They currently have {mutual} mutual entries." />
     <meta property="og:locale" content="en_GB" />
     <meta property="og:type" content="website" />
 </head>
 </html>
 """
 
+HEADINGS = ["romaji", "english", "native"]
+
 TABLE = """
 <table>
 <thead>
 <tr>
 <th>Media ID</th>
-<th>Romaji</th>
-<th>English</th>
-<th>Japanese</th>
+{included}
 <th>URL</th>
 </tr>
 </thead>
 <tbody>
-{body}
+{{body}}
 </tbody>
 </table>
 """
 
 ROW = """
 <tr>
-<td>{id}</td>
-<td>{title[romaji]}</td>
-<td>{title[english]}</td>
-<td>{title[native]}</td>
-<td><a href="{siteUrl}">Anilist</a></td>
+<td>{{id}}</td>
+{included}
+<td><a href="{{siteUrl}}">Anilist</a></td>
 </tr>
 """
 
@@ -102,19 +91,22 @@ class Status(Enum):
     repeating = "REPEATING"
 
 
-def format_entries_as_table(entries: dict[int, InnerMediaEntry]) -> str:
-    rows = [ROW.format_map(entry) for entry in entries.values()]
-    return TABLE.format(body="\n".join(rows))
+def format_entries_as_table(entries: dict[int, InnerMediaEntry], excluded: list[str]) -> str:
+    included = "\n".join(f"<td>{{title[{h}]}}</td>" for h in HEADINGS if h not in excluded)
+    rows = [ROW.format(included=included).format_map(entry) for entry in entries.values()]
+    formatted_headings = "\n".join(f"<th>{h.title()}</th>" for h in HEADINGS if h not in excluded)
+    return TABLE.format(included=formatted_headings).format(body="\n".join(rows))
 
 
 async def _fetch_user_entries(*usernames: str, status: Status) -> AnilistResponse | AnilistErrorResponse:
-    username1, username2 = usernames
+    parameters = ", ".join(f"$username{n}: String" for n in range(len(usernames)))
+    subqueries = "".join(USER_QUERY.format(number=n) for n in range(len(usernames)))
+    variables = {f"username{n}": name for n, name in enumerate(usernames)}
+    variables.update(status=status.value)
+    query = QUERY.format(parameters=parameters, subqueries=subqueries)
 
     async with httpx.AsyncClient() as session:
-        resp = await session.post(
-            "https://graphql.anilist.co",
-            json={"query": QUERY, "variables": {"username1": username1, "username2": username2, "status": status.value}},
-        )
+        resp = await session.post("https://graphql.anilist.co", json={"query": query, "variables": variables})
 
         return resp.json()
 
@@ -124,35 +116,44 @@ def _restructure_entries(entries: list[MediaEntry]) -> dict[int, InnerMediaEntry
 
 
 def _get_common_planning(data: AnilistResponse) -> dict[int, InnerMediaEntry]:
-    user1_data = data["data"]["user1"]["lists"]
-    user2_data = data["data"]["user2"]["lists"]
 
-    if not user1_data:
-        raise NoPlanningData(1)
-    elif not user2_data:
-        raise NoPlanningData(2)
+    media_entries: list[dict[int, InnerMediaEntry]] = []
 
-    user1_entries = _restructure_entries(user1_data[0]["entries"])
-    user2_entries = _restructure_entries(user2_data[0]["entries"])
+    for index, item in enumerate(data["data"].values()):
+        if not item:
+            raise NoPlanningData(index)
 
-    all_anime = user1_entries | user2_entries
-    common_anime = user1_entries.keys() & user2_entries.keys()
+        media_entries.append(_restructure_entries(item["lists"][0]["entries"]))
+
+    all_anime: dict[int, InnerMediaEntry] = reduce(or_, media_entries)
+    common_anime: set[int] = reduce(and_, map(lambda d: d.keys(), media_entries))
 
     return {id_: all_anime[id_] for id_ in common_anime}
 
 
-def _handle_errors(errors: list[AnilistError], user1: str, user2: str) -> list[str]:
+def _handle_errors(errors: list[AnilistError], *users: str) -> list[str]:
     missing_users: list[str] = []
     for error in errors:
         if error["message"] == "User not found" and error["status"] == 404:
             for location in error["locations"]:
-                print(location, flush=True)
-                if location["line"] == 3:
-                    missing_users.append(user1)
-                elif location["line"] == 18:
-                    missing_users.append(user2)
-
+                line = location["line"] - 4
+                index = line // len(USER_QUERY.splitlines())
+                missing_users.append(users[index])
     return missing_users
+
+
+def _human_join(seq: Sequence[str], /, *, delimiter: str = ", ", final: str = "and") -> str:
+    size = len(seq)
+    if size == 0:
+        return ""
+
+    if size == 1:
+        return seq[0]
+
+    if size == 2:
+        return f"{seq[0]} {final} {seq[1]}"
+
+    return delimiter.join(seq[:-1]) + f" {final} {seq[-1]}"
 
 
 @get("/")
@@ -164,12 +165,24 @@ async def index() -> Response[str]:
     )
 
 
-@get("/{user1:str}/{user2:str}")
-async def get_matches(user1: str, user2: str, status: str = "planning") -> Response[str]:
-    if user1.casefold() == user2.casefold():
+@get("/{user_list:path}")
+async def get_matches(user_list: str, exclude: list[str] | None = None, status: str = "planning") -> Response[str]:
+    users = list(set([user.casefold() for user in user_list.lstrip("/").split("/")]))
+
+    if len(users) <= 1:
         return Response(
-            "Haha, you're really funny.", media_type=MediaType.TEXT, status_code=status_codes.HTTP_418_IM_A_TEAPOT
+            f"Only {len(users)} user(s) provided. You must provide at least two, for example: <url>/user1/user2/etc...",
+            media_type=MediaType.TEXT,
+            status_code=status_codes.HTTP_400_BAD_REQUEST,
         )
+
+    for user in users:
+        if not user.isalnum() or len(user) > 20:
+            return Response(
+                f"User {user} is not a valid AniList username.",
+                media_type=MediaType.TEXT,
+                status_code=status_codes.HTTP_400_BAD_REQUEST,
+            )
 
     try:
         selected_status = Status[status.casefold()]
@@ -177,10 +190,21 @@ async def get_matches(user1: str, user2: str, status: str = "planning") -> Respo
         _statuses = "\n".join(item.name for item in Status)
         return Response(f"Sorry, your chosen status of {status} is not valid. Please choose from:-\n\n{_statuses}")
 
-    data = await _fetch_user_entries(user1.casefold(), user2.casefold(), status=selected_status)
+    excluded = list(set([ex.casefold() for ex in exclude or []]))
+
+    faulty = [ex for ex in excluded if ex not in HEADINGS]
+
+    if faulty:
+        return Response(
+            f"Unknown excluded headings: {_human_join(faulty)}. Supported: {_human_join(HEADINGS)}",
+            media_type=MediaType.TEXT,
+            status_code=status_codes.HTTP_400_BAD_REQUEST,
+        )
+
+    data = await _fetch_user_entries(*users, status=selected_status)
 
     if errors := data.get("errors"):
-        errored_users = _handle_errors(errors, user1, user2)
+        errored_users = _handle_errors(errors, *users)
 
         fmt = ", ".join(errored_users)
         return Response(
@@ -192,7 +216,7 @@ async def get_matches(user1: str, user2: str, status: str = "planning") -> Respo
     try:
         matching_items = _get_common_planning(data)  # type: ignore # the type is resolved above.
     except NoPlanningData as err:
-        errored_user = user1 if err.user == 1 else user2
+        errored_user = users[err.user]
         return Response(
             f"Sorry, but {errored_user} has no {selected_status.value.lower()} entries!",
             media_type=MediaType.TEXT,
@@ -206,8 +230,8 @@ async def get_matches(user1: str, user2: str, status: str = "planning") -> Respo
             status_code=status_codes.HTTP_412_PRECONDITION_FAILED,
         )
 
-    head = OPENGRAPH_HEAD.format(user1=user1, user2=user2, mutual=len(matching_items), status=selected_status.value.title())
-    formatted = format_entries_as_table(matching_items)
+    head = OPENGRAPH_HEAD.format(users=_human_join(users), mutual=len(matching_items), status=selected_status.value.title())
+    formatted = format_entries_as_table(matching_items, excluded=excluded)
 
     return Response(head + "\n" + formatted, media_type=MediaType.HTML, status_code=status_codes.HTTP_200_OK)
 
@@ -219,5 +243,6 @@ RL_CONFIG = RateLimitConfig(
     rate_limit_remaining_header_key="X-Ratelimit-Remaining",
     rate_limit_reset_header_key="X-Ratelimit-Reset",
 )
+
 
 app = Litestar(route_handlers=[index, get_matches], middleware=[RL_CONFIG.middleware])

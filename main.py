9302a63112245f573bc5d4b85fc9902e7b0317ea
Alex Nørgaard <umbra@abstractumbra.dev>
2024-07-14 19:55:15 +0100

    make uvloop optional

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import uvicorn
 
+has_uvloop = True
+try:
+    import uvloop as uvloop
+except ModuleNotFoundError:
+    has_uvloop = False
+
+
 if __name__ == "__main__":
     uvicorn.run(
         "anilist-cmp:app",
         host="0.0.0.0",
         port=8080,
         workers=2,
-        loop="uvloop",
+        loop="uvloop" if has_uvloop else "auto",
         proxy_headers=True,
         server_header=True,
         date_header=True,
